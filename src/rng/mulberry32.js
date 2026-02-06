7aa1c771ba351466bb06c2b4e02046d00a7e6fd3
Vinay <58536810+vinayplusj@users.noreply.github.com>
2026-02-06 10:26:14 -0500

    Create mulberry32.js
    ES module split

---
+++ src/rng/mulberry32.js
@@ -0,0 +1,9 @@
+export function mulberry32(seed) {
+  let t = seed >>> 0;
+  return function () {
+    t += 0x6D2B79F5;
+    let r = Math.imul(t ^ (t >>> 15), 1 | t);
+    r ^= r + Math.imul(r ^ (r >>> 7), 61 | r);
+    return ((r ^ (r >>> 14)) >>> 0) / 4294967296;
+  };
+}
