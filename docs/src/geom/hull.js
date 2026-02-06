f2be80eb8094b4369cc78a8b23f4e585a042a0e3
Vinay <58536810+vinayplusj@users.noreply.github.com>
2026-02-06 10:55:15 -0500

    Create hull.js
    ES module split

---
+++ docs/src/geom/hull.js
@@ -0,0 +1,32 @@
+// src/geom/hull.js
+// Convex hull (Monotone chain). Matches your inlined convexHull(points).
+
+function cross(o, a, b) {
+  return (a.x - o.x) * (b.y - o.y) - (a.y - o.y) * (b.x - o.x);
+}
+
+export function convexHull(points) {
+  const pts = (points || []).slice().sort((a, b) => (a.x - b.x) || (a.y - b.y));
+  if (pts.length <= 2) return pts;
+
+  const lower = [];
+  for (const p of pts) {
+    while (lower.length >= 2 && cross(lower[lower.length - 2], lower[lower.length - 1], p) <= 0) {
+      lower.pop();
+    }
+    lower.push(p);
+  }
+
+  const upper = [];
+  for (let i = pts.length - 1; i >= 0; i--) {
+    const p = pts[i];
+    while (upper.length >= 2 && cross(upper[upper.length - 2], upper[upper.length - 1], p) <= 0) {
+      upper.pop();
+    }
+    upper.push(p);
+  }
+
+  upper.pop();
+  lower.pop();
+  return lower.concat(upper);
+}
