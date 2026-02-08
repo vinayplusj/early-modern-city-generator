a6bab97a1028e5e580953d2c8c44d63804d64b3a
Vinay <58536810+vinayplusj@users.noreply.github.com>
2026-02-08 15:31:30 -0500

    Create background.js
    Refactor

---
+++ docs/src/render/stages/background.js
@@ -0,0 +1,15 @@
+// docs/src/render/stages/background.js
+
+export function drawBackground(ctx) {
+  // Background (robust clear even if caller applied transforms)
+  const cw = ctx.canvas.width;
+  const ch = ctx.canvas.height;
+
+  ctx.save();
+  ctx.setTransform(1, 0, 0, 1, 0, 0);
+  ctx.globalAlpha = 1.0;
+  ctx.clearRect(0, 0, cw, ch);
+  ctx.fillStyle = "#0f0f0f";
+  ctx.fillRect(0, 0, cw, ch);
+  ctx.restore();
+}
