228edb6e7ee23f14fefeeefa4f982134b4be7b94
Vinay <58536810+vinayplusj@users.noreply.github.com>
2026-02-27 17:59:28 -0500

    Update warpfield_draw_hints.js

--- docs/src/render/stages/warpfield_draw_hints.js
+++ docs/src/render/stages/warpfield_draw_hints.js
@@ -28,7 +28,7 @@ export function applyWarpfieldDrawHints({ warpWall, warpOutworks }) {
     // Final composite wall (bastioned outline)
     warpWall.drawComposite = {
       stroke: "#6e8190", // normal wall grey (pick what you want)
-      width: 10,
+      width: 20,
     };
   }
 
