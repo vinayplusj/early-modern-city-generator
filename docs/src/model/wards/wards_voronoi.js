a8fa3cb0a34aafe12dade4b71af390d8f8fd4d52
Vinay <58536810+vinayplusj@users.noreply.github.com>
2026-02-27 21:14:05 -0500

    Update wards_voronoi.js

--- docs/src/model/wards/wards_voronoi.js
+++ docs/src/model/wards/wards_voronoi.js
@@ -218,7 +218,7 @@ function normaliseParams(params) {
 
   return {
     seedCount,
-    spiralScale: numberOr(params?.spiralScale, 38),
+    spiralScale: numberOr(params?.spiralScale, 48),
     jitterRadius: numberOr(params?.jitterRadius, 10),
     jitterAngle: numberOr(params?.jitterAngle, 0.25),
     bboxPadding: numberOr(params?.bboxPadding, 250),
