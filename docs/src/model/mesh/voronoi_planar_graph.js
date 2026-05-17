1b98d0d4eb9a9a2767a7c79625440da608134e1c
Vinay <58536810+vinayplusj@users.noreply.github.com>
2026-05-17 19:44:32 -0400

    Delete docs/src/model/mesh/voronoi_planar_graph.js
    Remove voronoi_planar_graph.js wrapper

--- docs/src/model/mesh/voronoi_planar_graph.js
+++
@@ -1,6 +0,0 @@
-// docs/src/model/mesh/voronoi_planar_graph.js
-//
-// Thin re-export wrapper to keep import paths stable.
-
-export { buildVoronoiPlanarGraph } from "./voronoi_planar_graph/build.js";
-export { snapPointToGraph } from "./voronoi_planar_graph/snap.js";
